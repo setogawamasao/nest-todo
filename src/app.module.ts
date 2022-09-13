d61380d300cc776103dd8c345613008f1b81b4da
masao.setogawa <ms.job.hunting@gmail.com>
2022-09-14 01:33:58 +0900

    Add resource todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       logging: true,
       cache: false,
     }),
+    TodoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
