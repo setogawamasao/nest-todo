521f0dabadf4b6a784413939c37216592c4b125b
masao.setogawa <ms.job.hunting@gmail.com>
2022-09-14 01:27:17 +0900

    Add orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'todo-user',
+      password: 'todo-pass',
+      database: 'todo-db',
+      entities: ['dist/entities/**/*.entity.js'],
+      migrations: ['dist/migrations/**/*.js'],
+      synchronize: true,
+      logging: true,
+      cache: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
