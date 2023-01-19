6a41633f7ba5f9583b5c9981c3b70c605823f736
masao.setogawa <ms.job.hunting@gmail.com>
2023-01-20 00:18:47 +0900

    ユーザーモジュール追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
 import { dataSource } from './config/ormconfig';
+import { UserModule } from './user/user.module';
 console.log(dataSource.options);
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSource.options as TypeOrmModuleOptions),
     TodoModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
