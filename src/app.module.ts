0086ebb1705cbc1c98812b7be6c65b4d4794d3f9
masao.setogawa <ms.job.hunting@gmail.com>
2023-02-05 18:08:16 +0900

    authモジュールを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
 import { dataSource } from './config/ormconfig';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 console.log(dataSource.options);
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSource.options as TypeOrmModuleOptions),
     TodoModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
